58118e1b61604579ad751d2b4860ee743c2bce33
Tanvir Ahmed Rajib <rajib0909@gmail.com>
2021-06-22 17:08:00 +0600

    Project running

---
+++ app/src/main/java/com/example/assessmenttest/view/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.assessmenttest.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.assessmenttest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
